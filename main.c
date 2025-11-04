fe5c609f2f46535279bf073d1117b48ca260cc76
ycatanzaro <ycatanzaro@ant-neuro.com>
2025-11-04 18:19:56 +0100

    primeZahl

--- main.c
+++ main.c
@@ -33,6 +33,7 @@
 #include "examplesForUnderstanding/enumeration.h"
 #include "examplesForUnderstanding/doSmth.h"
 #include "examplesForUnderstanding/zweiTeiler.h"
+#include "examplesForUnderstanding/primeZahl.h"
 int main(int argc, char *argv[]) {
     /*
     helloFromC1();
@@ -64,7 +65,8 @@ int main(int argc, char *argv[]) {
    // ifStatement();
     //switchCase1();
     //enumeration();
-    doSmth();
-    zweiTeiler();
+    //doSmth();
+    //zweiTeiler();
+    primeZahl();
     return 0;
 }
