198c46c9ac0566e704eda2148625e5da775a7abc
ycatanzaro <ycatanzaro@ant-neuro.com>
2025-11-04 18:31:54 +0100

    faculR

---
+++ examplesForUnderstanding/facultaet.h
@@ -0,0 +1,8 @@
+//
+// Created by yfoljanty on 04/11/2025.
+//
+
+#ifndef GIVIL_UND_SEIN_C_FACULTAET_H
+#define GIVIL_UND_SEIN_C_FACULTAET_H
+void facultaet(void);
+#endif //GIVIL_UND_SEIN_C_FACULTAET_H
